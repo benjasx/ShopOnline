476fb67d9d9b892a89dcb2e157f8537fb3d2e78f
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-10 10:32:34 -0700

    SEED de usuarios, productos e imágenes

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -8,7 +8,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth(ValidRoles.admin)
+  /* @Auth(ValidRoles.admin) */
   executedSeed() {
     return this.seedService.runSeed();
   }

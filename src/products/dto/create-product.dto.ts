265f2e5f303658f34642d1c0c4e7868d9871d27e
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-11 10:25:49 -0700

    Documentar DTOs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,79 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'The title of the product',
+    example: 'Mens 3D T Logo Tee',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: 'The price of the product',
+    example: 29.99,
+    nullable: false,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    description: 'The description of the product',
+    example: 'A comfortable and stylish t-shirt for',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: 'The slug of the product',
+    example: 'mens_3d_t_logo_tee',
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    description: 'The stock of the product',
+    example: 100,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    description: 'The sizes available for the product',
+    example: ['S', 'M', 'L', 'XL'],
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'The gender for which the product is intended',
+    example: 'men',
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    description: 'The tags associated with the product',
+    example: ['t-shirt', 'cotton', 'summer'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    description: 'The images of the product',
+    example: ['http://example.com/image1.jpg', 'http://example.com/image2.jpg'],
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()

7f8243ab1400d5efc5534262468e6efdb3cb9f31
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-11 10:11:58 -0700

    Expandir el ApiProperty

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Product } from '../../products/entities/product.entity';
 import {
   BeforeInsert,
@@ -11,33 +12,63 @@ import {
 // Tener relacion en bbdd a la tabla de usuarios, para autenticar.
 @Entity('users')
 export class User {
+  @ApiProperty({
+    example: '227756fa-17c8-48c9-bada-ff124b46c895',
+    description: 'User ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'test1@google.com',
+    description: 'User Email',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   email: string;
 
+  @ApiProperty({
+    example: '$2b$10$S5wnvbqMyuxAyUFsPkhaBu0JeqE5HxKmVvbEkzYrfJVf/BbzO52Bu',
+    description: 'User Password',
+  })
   @Column('text', {
     select: false,
   })
   password: string;
 
+  @ApiProperty({
+    example: 'Benja Sanchez',
+    description: 'User FullName',
+  })
   @Column('text')
   fullName: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'User is Active',
+  })
   @Column('bool', {
     default: true,
   })
   isActive: boolean;
 
+  @ApiProperty({
+    example: ['user', 'admin'],
+    description: 'User Roles',
+  })
   @Column('text', {
     array: true,
     default: ['user'],
   })
   roles: string[];
 
+  @ApiProperty({
+    example: '8765120-00-A_0_2000.jpg',
+    description: 'User Product',
+  })
   @OneToMany(() => Product, (product) => product.user)
   product: Product;
 
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -15,52 +15,84 @@ import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
-  @ApiProperty()
+  @ApiProperty({
+    example: '227756fa-17c8-48c9-bada-ff142cvdfc895',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Mens 3D T Logo Tee',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 0,
+    description: 'Product Price',
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Cillum magna qui consectetur sint consectetur nostrud veniam',
+    description: 'Product Description',
+    default: null,
+  })
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'mens_3d_t_logo_tee',
+    description: 'Product Slug - For SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stok',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['XS', 'S'],
+    description: 'Product sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'men',
+    description: 'Product Gender',
+  })
   @Column('text')
   gender: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['shirt'],
+    description: 'Product Tags',
+  })
   @Column('text', {
     array: true,
     default: [],
@@ -68,7 +100,10 @@ export class Product {
   tags: string[];
 
   // images
-  @ApiProperty()
+  @ApiProperty({
+    example: ['8765120-00-A_0_2000.jpg', '8765120-00-A_1.jpg'],
+    description: 'Product Images',
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,

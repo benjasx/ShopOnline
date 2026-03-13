c673a8d1cf23592b787bfc069871477c0c084a46
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-13 10:30:05 -0700

    Preparar cliente para enviar JWT

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -17,6 +17,9 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
     /* console.log('Cliente conectado:', client.id); */
+    const token = client.handshake.headers.autentication as string;
+    console.log({ token });
+
     this.messagesWsService.registerClient(client);
     this.wss.emit(
       'clients-update',
